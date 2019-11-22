2ad4f052c6676b5a517cd3b434f5ab98dc9fb356
Erovia <erovia@users.noreply.github.com>
2019-11-22 22:15:17 +0100

    Refactor for parameter precedence
    This commit adds support for the following parameter precedence:
    command-line > context > config

--- lib/python/qmk/cli/compile.py
+++ lib/python/qmk/cli/compile.py
@@ -34,15 +34,11 @@ def compile(cli):
     qmk_path = os.getcwd()
     current_folder = os.path.basename(cwd)
     # Initialize boolean to check for being in a keyboard directory and initialize keyboard string
-    in_keyboard = False
-    in_layout = False
     keyboard = ""
     keymap = ""
-    user_keymap = ""
-    user_keyboard = ""
 
     # Set path for '/keyboards/' directory
-    keyboards_path = os.path.join(qmk_path , "keyboards")
+    keyboards_path = os.path.join(qmk_path, "keyboards")
     layouts_path = os.path.join(qmk_path, "layouts")
 
     # If below 'keyboards' and not in 'keyboards' or 'keymaps', get current keyboard name
@@ -56,64 +52,41 @@ def compile(cli):
             else:
                 keyboard = str(cwd[len(keyboards_path):])[1:]
 
-            in_keyboard= True
+    # If compile keyboard are supplied, assign those
+    if cli.args.keyboard:
+        keyboard = cli.args.keyboard
+    # config keyboard
+    elif cli.config.compile.keyboard and not keyboard:
+        keyboard = cli.config.compile.keyboard
 
-    # If in layouts dir
-    if cwd.startswith(layouts_path):
-        if current_folder != "layouts":
-            layout = str(cwd[len(layouts_path):])[1:]
-            in_layout=True
-
-    # If user keyboard/keymap or compile keyboard/keymap are supplied, assign those
-    if cli.config.compile.keyboard or cli.config.user.keyboard:
-        user_keyboard = cli.config.user.keyboard if cli.config.user.keyboard else cli.config.compile.keyboard
-    if cli.config.compile.keymap or cli.config.user.keymap:
-        if not in_layout:
-            user_keymap = cli.config.user.keymap if cli.config.user.keymap else cli.config.compile.keymap 
+    # If compile keymap are supplied, assign those
+    if cli.args.keymap:
+        keymap = cli.args.keymap
+    elif not keymap:
+        # If in layouts dir
+        if cwd.startswith(layouts_path):
+            if current_folder != "layouts":
+                keymap = current_folder
+        # If user keymap are supplied, assign those
+        elif cli.config.compile.keymap:
+            keymap = cli.config.compile.keymap
 
     if cli.args.filename:
         # Parse the configurator json
-        user_keymap = parse_configurator_json(cli.args.filename)
+        keymap = parse_configurator_json(cli.args.filename.name)
 
         # Generate the keymap
-        keymap_path = qmk.path.keymap(user_keymap['keyboard'])
-        cli.log.info('Creating {fg_cyan}%s{style_reset_all} keymap in {fg_cyan}%s', user_keymap['keymap'], keymap_path)
+        keymap_path = qmk.path.keymap(keymap['keyboard'])
+        cli.log.info('Creating {fg_cyan}%s{style_reset_all} keymap in {fg_cyan}%s', keymap['keymap'], keymap_path)
 
         # Compile the keymap
-        command = compile_configurator_json(cli.args.filename)
+        command = compile_configurator_json(cli.args.filename.name)
 
-        cli.log.info('Wrote keymap to {fg_cyan}%s/%s/keymap.c', keymap_path, user_keymap['keymap'])
+        cli.log.info('Wrote keymap to {fg_cyan}%s/%s/keymap.c', keymap_path, keymap['keymap'])
 
-    elif user_keyboard and user_keymap:
+    elif keyboard and keymap:
         # Generate the make command for a specific keyboard/keymap.
-        command = create_make_command(user_keyboard, user_keymap)
-
-    elif in_keyboard:
-        keyboard = user_keyboard if user_keyboard else keyboard
-        keymap = user_keymap if user_keymap else keymap
-        
-        if not os.path.exists(os.path.join(keyboards_path, keyboard, "rules.mk")):
-            cli.log.error('This directory does not contain a rules.mk file. Change directory or supply --keyboard with optional --keymap')
-            return False
-        # Get path for keyboard directory
-        keymap_path = qmk.path.keymap(keyboard)
-        # Check for global keymap config first
-        if keymap:
-            command = create_make_command(keyboard, keymap)
-
-        else:
-            # If no default keymap exists and none provided
-            cli.log.error('This directory does not contain a keymap. Set one with `qmk config` or supply `--keymap` ')
-            return False
-
-    elif in_layout:
-        if user_keyboard:
-            keymap = current_folder
-            command = create_make_command(user_keyboard, keymap)
-        else:
-            cli.log.error('You must supply a keyboard to compile a layout keymap. Set one with `qmk config` or supply `--keyboard` ')
-            return False
-
+        command = create_make_command(keyboard, keymap)
 
     else:
         cli.log.error('You must supply a configurator export, both `--keyboard` and `--keymap`, or be in a directory for a keyboard or keymap.')
