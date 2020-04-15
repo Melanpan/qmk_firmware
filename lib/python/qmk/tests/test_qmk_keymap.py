41257356f596e81f01199bbd4489091d3db6dd0a
Erovia <erovia@users.noreply.github.com>
2020-04-15 23:29:30 +0200

    Add tests

--- lib/python/qmk/tests/test_qmk_keymap.py
+++ lib/python/qmk/tests/test_qmk_keymap.py
@@ -13,7 +13,7 @@ def test_template_onekey_proton_c_json():
 
 def test_template_onekey_pytest():
     templ = qmk.keymap.template('handwired/onekey/pytest')
-    assert templ == 'const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {__KEYMAP_GOES_HERE__};\n'
+    assert templ == '#include QMK_KEYBOARD_H\n\nconst uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {__KEYMAP_GOES_HERE__};\n'
 
 
 def test_template_onekey_pytest_json():
@@ -23,12 +23,17 @@ def test_template_onekey_pytest_json():
 
 def test_generate_onekey_pytest():
     templ = qmk.keymap.generate('handwired/onekey/pytest', 'LAYOUT', [['KC_A']])
-    assert templ == 'const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {	[0] = LAYOUT(KC_A)};\n'
+    assert templ == '#include QMK_KEYBOARD_H\n\nconst uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {	[0] = LAYOUT(KC_A)};\n'
 
 
 def test_generate_onekey_pytest_json():
     templ = qmk.keymap.generate('handwired/onekey/pytest', 'LAYOUT', [['KC_A']], type='json')
     assert templ == {'keyboard': 'handwired/onekey/pytest', 'layout': 'LAYOUT', 'layers': [['KC_A']]}
+
+
+def test_parse_keymap_c():
+    parsed_keymap_c = qmk.keymap.parse_keymap_c('keyboards/handwired/onekey/keymaps/pytest/keymap.c')
+    assert parsed_keymap_c == {'layers': [{'name': '0', 'layout': 'LAYOUT', 'keycodes': ['KC_ENTER']}]}
 
 
 # FIXME(skullydazed): Add a test for qmk.keymap.write that mocks up an FD.
